29924f6260c5f3ca0c13c0a8af44ed4348beef98
Padlibzz <padlibadzara@gmail.com>
2025-11-30 13:10:43 +0700

    fix

--- Pertemuan2/TUGAS_P2_BACKEND/T1.php
+++ Pertemuan2/TUGAS_P2_BACKEND/T1.php
@@ -1,20 +1,32 @@
 <?php
 //Buat program yang menampilkan bilangan genap 1–20.
-echo "Bilangan Bulat 1-20 : ";
-for ($i = 1; $i <= 20; $i++) {
-  echo "$i ";
-}
-echo "<br>";
-echo "Bilangan genap 1-20 : ";
-for ($i = 1; $i <= 20; $i++) {
-  if ($i % 2 == 0) {
+function BBUlat()
+{
+  echo "Bilangan Bulat 1-20 : ";
+  for ($i = 1; $i <= 20; $i++) {
     echo "$i ";
   }
 }
-echo "<br>";
-echo "Bilangan ganjil 1-20 : ";
-for ($i = 1; $i <= 20; $i++) {
-  if ($i % 2 != 0) {
-    echo "$i ";
+
+function BGenap()
+{
+  echo "Bilangan genap 1-20 : ";
+  for ($i = 1; $i <= 20; $i++) {
+    if ($i % 2 == 0) {
+      echo "$i ";
+    }
   }
 }
+function BGanjil()
+{
+  echo "Bilangan ganjil 1-20 : ";
+  for ($i = 1; $i <= 20; $i++) {
+    if ($i % 2 != 0) {
+      echo "$i ";
+    }
+  }
+}
+
+echo BBUlat() . "<br>";
+echo BGenap() . "<br>";
+echo BGanjil() . "<br>";
